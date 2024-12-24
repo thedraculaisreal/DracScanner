d629f9f740224ed91a4ee2f1868060ea2485ba0e
thedraculaisreal <jakejackson2546@gmail.com>
2024-12-24 15:24:53 +0000

    fixed scanner, displays ports that are open at end.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DracScanner
+
+Trys to establish a tcp connection, if it can it registers the port as open.
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
-use std::io::prelude::*;
+use std::env;
 use std::net::TcpStream;
 
-fn main() std::io::Result<()> {
+fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
+    let mut ports = Vec::new();
     let ip = args[1].clone();
-    let mut port = 0;
+    let mut port = 20;
     while port < 65535 {
-	let mut stream = TcpStream::connect("{ip}:{port}")?;
+	if let Ok(stream) = TcpStream::connect("{ip}:{port}"){
+	    println!("{port} is open");
+	    ports.push(port);
+	}
+	else {
+	    println!("{port} is closed");
+	}
+	port += 1;
+    }
+
+    println!("************************");
+
+    for port in ports {
+	println!("{port} is open")
     }
 
     Ok(())
